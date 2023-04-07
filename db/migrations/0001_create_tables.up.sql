e0f32f751aedba7a3852a94e46c5fe9e57b7a656
Lukas <l.heindl+git@protonmail.com>
2023-04-07 21:48:22 +0200

    fix typo in migration

--- db/migrations/0001_create_tables.up.sql
+++ db/migrations/0001_create_tables.up.sql
@@ -1,25 +1,25 @@
 START TRANSACTION;
 CREATE TABLE IF NOT EXISTS categories (
   name VARCHAR(255) NOT NULL,
-  PRIMARY KEY (name)
+  CONSTRAINT pk_cat PRIMARY KEY (name)
 );
 CREATE TABLE IF NOT EXISTS item_types (
   name varchar(255) NOT NULL,
   category_name varchar(255) NOT NULL,
-  PRIMARY KEY (name),
-  FOREIGN KEY (category_name) REFERENCES categories(name)
+  CONSTRAINT pk_item_types PRIMARY KEY (name),
+  CONSTRAINT fk_item_types_cat FOREIGN KEY (category_name) REFERENCES categories(name)
     ON UPDATE CASCADE
     ON DELETE RESTRICT
 );
 CREATE TABLE IF NOT EXISTS freezer_items (
   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
   date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  identifier varchar(255),
-  amount varchar(255),
-  misc varchar(255),
-  item_name varchar(255),
-  PRIMARY KEY (id),
-  FOREIGN KEY (item_name) REFERENCES item_types(name)
+  identifier varchar(255) NOT NULL,
+  amount varchar(255) NOT NULL,
+  misc varchar(255) NOT NULL,
+  item_name varchar(255) NOT NULL,
+  CONSTRAINT pk_freezer_items PRIMARY KEY (id),
+  CONSTRAINT fk_freezer_items_name FOREIGN KEY (item_name) REFERENCES item_types(name)
     ON UPDATE CASCADE
     ON DELETE RESTRICT
 	);
